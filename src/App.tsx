c8686c7c0064e1e040de379a25c76ae299a33180
AMER\kevin.freistroffer <kevin.freistroffer@theromofisher.com>
2020-09-18 16:14:25 -0700

    project created with create-react-app typescript template

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React Components</h1>
+        <button>Can't wait to &gt;&gt;&gt;</button> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
